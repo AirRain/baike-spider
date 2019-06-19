4902ea607fc110151a67445da3cd0f86eea63ae5
AirRain <31362594+airrain@users.noreply.github.com>
2019-06-19 16:56:01 +0800

    Create guess-number.py

---
+++ game/guess-number.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import random
+number = random.randint(0,1024)
+running = True
+num = 0
+nmaxn = 1024
+nminn = 0
+
+def eBtnClose(event):
+    root.destroy()
+def eBtnGuess(event):
+    global nmaxn
+    global nminn
+    global num
+    global running
+    if running:
+        
+
